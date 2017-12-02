d932c911e911591810a6d5b7589db32c16aa5796
Robert Henning <roberthenning.188@gmail.com>
2017-12-02 17:05:48 -0500

    Dispenses mango and milk
    has dispensing screen, needs the redundant arduino load to be fixed

---
+++ app/src/main/java/com/example/root/lambda/dispensing.java
@@ -0,0 +1,13 @@
+package com.example.root.lambda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dispensing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dispensing);
+    }
+}
